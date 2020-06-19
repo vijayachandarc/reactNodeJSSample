9b26265e7a1977dd9eb2dd38d90b36aff5a2a7dc
vijayachandarc <52036322+vijayachandarc@users.noreply.github.com>
2020-06-19 12:17:53 +1000

    Hello world

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
 console.log('Bot started');
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+
+//
+//  tweet 'hello world!'
+//
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
